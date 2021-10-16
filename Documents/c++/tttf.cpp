8578b49ca85ffc23d059a2a170edabf0eda256ae
luca cardi <lucacardi@pop-os.localdomain>
2021-10-16 18:53:05 +0100

    check who won and tie

--- Documents/c++/tttf.cpp
+++ Documents/c++/tttf.cpp
@@ -9,14 +9,13 @@ int place;
 char player1 = 'X';
 char player2 = '0';   
 
-
 void description() {
   std::cout << "=============================\n";
   std::cout << "welcome to a tic tac toe game\n";
   std::cout << "=============================\n\n";
   std::cout << "I guess you know the rules ;)\n\n";
 }
-
+// the board
 void draw()  {
 
  
@@ -28,8 +27,9 @@ void draw()  {
   std::cout << "     |     |      \n";
 
 }
-
+// how you select the place where to put X or 0
 void selection() {
+ 
   while (moves <9) {
     
     if (moves % 2 == 0) {
@@ -68,16 +68,21 @@ void selection() {
     }
     game();
     moves += 1;
-    
+   } std::cout << "It's a tie!!";
   }
-}
+ 
 
+
+
+
+
+// check if someone won
 void game() {
   
-    if (board[0] != ' ') {
-    if ((board[0] == board[1] && board[1] == board[2] )
+    if (((board[0] == board[1] && board[1] == board[2] )
      || (board[0] == board[3] && board[3] == board[6] ) 
-     || (board[0] == board[4] && board[4] == board[8] )) {
+     || (board[0] == board[4] && board[4] == board[8] )) 
+     && (board[0] != ' ')) {
 
     if (board[0] == player1)  {
       
@@ -88,11 +93,50 @@ void game() {
       
       std::cout << "Congratulation player 2, you won!!\n";
       moves=+ 10;
+  } 
+ }  
+
+   if (((board[6] == board[4] && board[4] == board[2] )
+    || (board[6] == board[7] && board[7] == board[8] ))
+    && (board[6] != ' '))  {
+      if (board[6] == player1)  {
+      
+      std::cout << "Congratulation player 1, you won!!\n";
+      moves=+ 10;
+
+    } else {
+      
+      std::cout << "Congratulation player 2, you won!!\n";
+      moves=+ 10;
+  }
+ } if (((board[1] == board[4] && board[4] == board[7] ) 
+    || (board[3] == board[4] && board[4] == board[5])) 
+    && (board[4] != ' ')) {
+    
+      if (board[1] == player1)  {
+      
+      std::cout << "Congratulation player 1, you won!!\n";
+      moves=+ 10;
+
+    } else {
+      
+      std::cout << "Congratulation player 2, you won!!\n";
+      moves=+ 10;
+   
+     }  
+    } if ((board[2] == board[5] && board[5] == board[8]) && (board[2] != ' '))  {
+      
+      if (board[2] == player1)  {
+  
+      std::cout << "Congratulation player 1, you won!!\n";
+      moves=+ 10;
+    } else {
+      
+      std::cout << "Congratulation player 2, you won!!\n";
+      moves=+ 10;
     } 
   }
- }
-}
-
+} 
  
 
 
